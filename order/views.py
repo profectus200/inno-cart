3def2d903259f473d57623b1f587a67e5cce287c
Vladimir <vladimir314v@gmail.com>
2022-06-28 12:23:50 +0300

    Delivery functionality added

--- order/views.py
+++ order/views.py
@@ -1,15 +1,22 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import OrderModel
-from .serializers import OrderSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from .models import OrderModel
+from .serializers import OrderSerializer
 
-# Create your views here.
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = OrderModel.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('status',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(status='CREATED')
+        return queryset
 
 
 class PersonalOrderViewSet(viewsets.ModelViewSet):
@@ -22,4 +29,17 @@ class PersonalOrderViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+
+class DeliveryViewSet(viewsets.ModelViewSet):
+    queryset = OrderModel.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('deliverer_id',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(deliverer_id=self.request.user.id)
         return queryset
